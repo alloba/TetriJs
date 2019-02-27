14d783db0747edd60d94c87f48636c1df55d189d
Alex <alexlbates@gmail.com>
2019-02-27 11:46:58 -0500

    modified piece movement functions -- removed need to pass board in. added a moveUp function to Piece, added moveUp behavior when rotating

--- scripts/Tetri2Js.js
+++ scripts/Tetri2Js.js
@@ -15,12 +15,12 @@ class Piece {
         new Array(3).fill(null),
         new Array(3).fill(null)
     ];
-    position = {x: 0, y: 0}; // upper left of piece's grid
+    position = { x: 0, y: 0 }; // upper left of piece's grid
 
 
     constructor(board, blockShape) {
         if (board === null || board === undefined)
-            throw("No board provided instantiating new Piece object");
+            throw ("No board provided instantiating new Piece object");
         this.board = board;
 
         switch (blockShape) {
@@ -96,7 +96,7 @@ class Piece {
                 break;
             }
             default:
-                throw("Unsupported block type given to Piece object constructor");
+                throw ("Unsupported block type given to Piece object constructor");
         }
     }
 
@@ -115,11 +115,11 @@ class Piece {
         }
     };
 
-    proposeRotation() {
-        const newPiece = Piece.copyPiece(this.board, this);
+    static proposeRotation(piece) {
+        const newPiece = Piece.copyPiece(piece);
         let rotatedPieceComponents = [];
-        for(let i = 0; i < this.subPieces.length; i++)
-            rotatedPieceComponents.push(new Array(this.subPieces.length).fill(null))
+        for (let i = 0; i < piece.subPieces.length; i++)
+            rotatedPieceComponents.push(new Array(piece.subPieces.length).fill(null))
 
         let n = newPiece.subPieces.length;
         for (let i = 0; i < n; i++) {
@@ -134,22 +134,30 @@ class Piece {
     }
 
     rotate() {
-        const newPiece = this.proposeRotation();
-        if (!Piece.detectCollision(board, newPiece)) {
-            this.subPieces = newPiece.subPieces;
-            this.position = newPiece.position;
+        const originalPiece = Piece.copyPiece(this);
+        let rotatedPiece = Piece.proposeRotation(originalPiece);
+
+        while(Piece.detectCollision(rotatedPiece)){
+            rotatedPiece.moveUp()
         }
-    };
+
+        if (!Piece.detectCollision(rotatedPiece)) {
+            this.subPieces = rotatedPiece.subPieces;
+            this.position = rotatedPiece.position;
+        }
+    }
+
+
 
     proposeMoveDown() {
-        const newPiece = Piece.copyPiece(this.board, this);
+        const newPiece = Piece.copyPiece(this);
         newPiece.position.y = this.position.y + 1;
         return newPiece;
     }
 
     moveDown() {
         let newPiece = this.proposeMoveDown();
-        if (!Piece.detectCollision(this.board, newPiece)) {
+        if (!Piece.detectCollision(newPiece)) {
             this.subPieces = newPiece.subPieces;
             this.position = newPiece.position;
         }
@@ -157,36 +165,53 @@ class Piece {
 
     proposeMoveHorizontal(direction) {
         if (direction !== -1 && direction !== 1)
-            throw("invalid value passed to proposeMoveHorizontal. Must be either 1 or -1");
-        const newPiece = Piece.copyPiece(this.board, this);
+            console.trace("invalid value passed to proposeMoveHorizontal. Must be either 1 or -1");
+        const newPiece = Piece.copyPiece(this);
         newPiece.position.x += direction;
         return newPiece;
     }
 
     moveHorizontal(direction) {
         const newPiece = this.proposeMoveHorizontal(direction);
-        if (!Piece.detectCollision(this.board, newPiece))
+        if (!Piece.detectCollision(newPiece))
             this.position = newPiece.position;
     }
 
-    static detectCollision(board, piece) {
-        if (board === null || piece === null)
-            throw("Piece.detectCollision was not provided with a defined board and piece (check if you forgot the board)");
+    proposeMoveUp() {
+        const newPiece = Piece.copyPiece(this);
+        newPiece.position.y -= 1;
+        return newPiece;
+    }
+
+    moveUp() {
+        let newPiece = Piece.copyPiece(this);
+        do {
+            newPiece = newPiece.proposeMoveUp();
+        }
+        while (Piece.detectCollision(newPiece));
+
+        this.subPieces = newPiece.subPieces;
+        this.position = newPiece.position;
+    }
+
+    static detectCollision(piece) {
+        if (piece.board === null || piece === null)
+            console.trace("Piece.detectCollision was not provided with a defined piece");
         for (let i = 0; i < piece.subPieces.length; i++) {
             for (let j = 0; j < piece.subPieces.length; j++) {
                 if (piece.subPieces[i][j] !== null) {
                     //out of bounds vertically
-                    if (i + piece.position.y > board.unitSize.height - 1) {
+                    if (i + piece.position.y > piece.board.unitSize.height - 1) {
                         return true;
                     }
                     //out of bounds horizontally
-                    if (j + piece.position.x > board.unitSize.width - 1 || j + piece.position.x < 0) {
+                    if (j + piece.position.x > piece.board.unitSize.width - 1 || j + piece.position.x < 0) {
                         return true;
                     }
                     //overlapping any piece on game board.
-                    for (let by = 0; by < board.grid.length; by++) {
-                        for (let bx = 0; bx < board.grid[0].length; bx++) {
-                            if (board.grid[by][bx] !== null)
+                    for (let by = 0; by < piece.board.grid.length; by++) {
+                        for (let bx = 0; bx < piece.board.grid[0].length; bx++) {
+                            if (piece.board.grid[by][bx] !== null)
                                 if (by === i + piece.position.y && bx === j + piece.position.x) {
                                     return true;
                                 }
@@ -198,13 +223,16 @@ class Piece {
         return false;
     }
 
-    static copyPiece(board, piece) {
-        let newPiece = new Piece(board, Piece.BLOCK_TEMPLATE);
+    static copyPiece(piece) {
+        if (piece.board == undefined || piece == undefined)
+            console.trace("Missing either board or piece parameter in call to Piece.copyPiece");
+
+        let newPiece = new Piece(piece.board, Piece.BLOCK_TEMPLATE);
         newPiece.subPieces = [];
         for (let i = 0; i < piece.subPieces.length; i++)
             newPiece.subPieces.push(new Array(piece.subPieces.length).fill(null))
 
-        newPiece.position = {x: piece.position.x, y: piece.position.y};
+        newPiece.position = { x: piece.position.x, y: piece.position.y };
 
         for (let i = 0; i < piece.subPieces.length; i++)
             for (let j = 0; j < piece.subPieces.length; j++)
@@ -216,21 +244,21 @@ class Piece {
 }
 
 class Board {
-    unitSize = {width: 10, height: 20};
-    drawSize = {x: 400, y: 800};
-    grid = null; //new Array(20).fill(new Array(10).fill(null));
+    unitSize = { width: 10, height: 20 };
+    drawSize = { x: 400, y: 800 };
+    grid = null;
     context = null;
 
     constructor(context, width, height) {
         if (typeof context !== "object")
             throw ("Missing canvas context provided to Board object");
         if (width !== null && height !== null) {
-            this.unitSize = {width: width, height: height};
+            this.unitSize = { width: width, height: height };
         }
-        this.grid = [];//new Array(height).fill(new Array(width).fill(null));
-        for(let i = 0; i < height; i++) {
+        this.grid = [];
+        for (let i = 0; i < height; i++) {
             this.grid.push([]);
-            for(let j = 0; j < width; j++)
+            for (let j = 0; j < width; j++)
                 this.grid[i].push(null)
         }
 
@@ -246,7 +274,7 @@ class Board {
         const pieceDraw = this.drawSize.x / this.unitSize.width;
         for (let i = 0; i < this.grid.length; i++) {
             for (let j = 0; j < this.grid[0].length; j++) {
-                if(this.grid[i][j] !== null) {
+                if (this.grid[i][j] !== null) {
                     this.context.fillStyle = this.grid[i][j];
                     this.context.fillRect(pieceDraw * j, pieceDraw * i, pieceDraw, pieceDraw)
                 }
@@ -270,9 +298,9 @@ board.draw();
 activePiece.rotate();
 activePiece.rotate();
 board.grid[8][4] = "#FFF";
-canvas.addEventListener('click', function () {
-    tick();
-});
+// canvas.addEventListener('click', function () {
+//     tick();
+// });
 window.addEventListener('keydown', arrowKeysListener);
 
 
@@ -300,12 +328,18 @@ function arrowKeysListener(e) {
             break;
         case 38: //up
             console.log("up");
-            activePiece.rotate();
+            activePiece.moveUp();
             break;
         case 40: //down
             console.log("down");
             activePiece.moveDown();
             break;
+        case 32: //spacebar
+            console.log("spacebar -- rotate");
+            activePiece.rotate();
+            break;
+        default:
+            console.log(`Code ${e.keyCode} not registered`)
 
     }
     draw();
